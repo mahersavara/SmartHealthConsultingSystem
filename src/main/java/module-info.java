b47bc5ae09c5f276e821ba23344c69cc5ed77b90
Eric <kdlinh8811@gmail.com>
2024-06-01 13:11:47 +0700

    Initialise

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module org.example.smarthealthconsultingsystem {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.example.smarthealthconsultingsystem to javafx.fxml;
+    exports org.example.smarthealthconsultingsystem;
+}
