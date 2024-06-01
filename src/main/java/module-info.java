43256a156798f2d282ad8cb0efd972e4694bf633
Eric <kdlinh8811@gmail.com>
2024-06-01 17:33:29 +0700

    Initialise_work_not_style

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module org.example.smarthealthconsultingsystem {
     requires org.controlsfx.controls;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens org.example.smarthealthconsultingsystem to javafx.fxml;
     exports org.example.smarthealthconsultingsystem;
